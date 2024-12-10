82cb40cb3cc4c5842f02b156237b21a98363436d
Yoshi C. <den.yosh3@gmail.com>
2024-12-10 19:48:20 +0100

    Initial Commit

---
+++ user-service/src/test/java/com/drivetogether/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivetogether.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
