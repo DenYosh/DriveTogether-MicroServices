19c413b738ae9634015220014b688f5da33df629
Yoshi C. <den.yosh3@gmail.com>
2024-12-17 19:14:55 +0100

    Updated Environment

--- drivetogether-front/src/recoil/store.js
+++ drivetogether-front/src/recoil/store.js
@@ -1,14 +1,14 @@
 import { atom } from "recoil";
 
 export const ApiUrl =
-	process.env.REACT_APP_API_GATEWAY_BASEURL || "http://localhost:8084";
+	process.env.NODE_ENV == "production"
+		? "api-gateway:8084"
+		: "http://localhost:8084";
 
 export const RedirectUri =
-	process.env.REACT_APP_REDIRECT_URI || "http://localhost:5173/callback";
-
-console.log("process.env: ", process.env);
-console.log("ApiUrl: ", ApiUrl);
-console.log("RedirectUri: ", RedirectUri);
+	process.env.NODE_ENV == "production"
+		? "http://localhost/callback"
+		: "http://localhost:5173/callback";
 
 export const Bearertoken = atom({
 	key: "Bearertoken",
