73794fecf2b04c3caeff64ddfdbfb0e732b5d5ef
Yoshi C. <den.yosh3@gmail.com>
2024-12-21 12:08:04 +0100

    Test of env

--- vehicle-service/src/main/java/com/drivetogether/vehicleservice/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/com/drivetogether/vehicleservice/VehicleServiceApplication.java
@@ -2,12 +2,28 @@ package com.drivetogether.vehicleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class VehicleServiceApplication {
 
+    @Autowired
+    public void printEnvironment(Environment environment) {
+        System.out.println("### Active Profiles ###");
+        for (String profile : environment.getActiveProfiles()) {
+            System.out.println("Active Profile: " + profile);
+        }
+
+        System.out.println("### System Properties ###");
+        System.getProperties().forEach((key, value) -> System.out.println(key + " = " + value));
+
+        System.out.println("### Environment Variables ###");
+        System.getenv().forEach((key, value) -> System.out.println(key + " = " + value));
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(VehicleServiceApplication.class, args);
+//        SpringApplication.run(VehicleServiceApplication.class, args);
     }
 
 }
