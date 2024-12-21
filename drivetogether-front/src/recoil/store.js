f303aa48ef8e2f742156b92607fce9605b6a9575
Yoshi C. <den.yosh3@gmail.com>
2024-12-21 11:20:40 +0100

    Changed API URl Frontend

--- drivetogether-front/src/recoil/store.js
+++ drivetogether-front/src/recoil/store.js
@@ -1,9 +1,6 @@
 import { atom } from "recoil";
 
-export const ApiUrl =
-	process.env.NODE_ENV == "production"
-		? "api-gateway:8084"
-		: "http://localhost:8084";
+export const ApiUrl = "http://localhost:8084";
 
 export const RedirectUri =
 	process.env.NODE_ENV == "production"
